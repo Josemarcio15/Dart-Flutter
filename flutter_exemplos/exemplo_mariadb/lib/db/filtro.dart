f0a080700cada2af6c975357bd73d7dd8c940adf
Josemarcio15 <josemarcio0araujo@gmail.com>
2025-09-13 00:33:50 -0400

    FirstCommit

---
+++ flutter_exemplos/exemplo_mariadb/lib/db/filtro.dart
@@ -0,0 +1,19 @@
+import 'package:exemplo_mariadb/db/query.dart';
+// import 'package:mysql1/mysql1.dart';
+
+class Filtro {
+  Future<List<Map<String, dynamic>>> resultado() async {
+    var resposta = await busca();
+
+    List<Map<String, dynamic>> lista = [];
+    for (var row in resposta) {
+      lista.add({
+        "id": row["id"],
+        "name": row["name"],
+        "price": row["price"],
+        "stock": row["stock"],
+      });
+    }
+    return lista;
+  }
+}
--- flutter_exemplos/exemplo_mariadb/lib/pages/home.dart
+++ flutter_exemplos/exemplo_mariadb/lib/pages/home.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
-import '../db/query.dart'; // seu arquivo com a função busca()
+//import '../db/query.dart'; // seu arquivo com a função busca()
+import '../db/filtro.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final Filtro filtro = Filtro();
+  String textoBotao = "Iniciando";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Produtos')),
-      body: FutureBuilder(
-        future: busca(), // chama a função do banco
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Erro: ${snapshot.error}'));
-          }
-
-          final resultados = snapshot.data!;
-          return ListView.builder(
-            itemCount: resultados.length,
-            itemBuilder: (context, index) {
-              final row = resultados.elementAt(index);
-              return ListTile(
-                title: Text(row['name'].toString()),
-                subtitle: Text('Preço: R\$ ${row['price']}'),
-              );
+      body: Column(
+        children: [
+          // ListView.separated(
+          //   itemBuilder: filtro.resultado(),
+          //   separatorBuilder: "|",
+          //   itemCount: filtro.resultado().lenght,
+          // ),
+          ElevatedButton(
+            onPressed: () async {
+              List<Map<String, dynamic>> lista = await filtro.resultado();
+              print(lista[0]);
+              setState(() {
+                textoBotao = lista[0]["name"];
+              });
             },
-          );
-        },
+            child: Text(textoBotao),
+          ),
+        ],
       ),
     );
   }
